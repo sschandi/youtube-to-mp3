b2b83b2f61fe26379dbc916a21cf5029873d450a
Sukhdeep Chandi <sukhdeep@sarkedev.com>
2019-01-08 17:50:32 -0800

    use ffmpeg to add metadata to file

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <p>{{ savePath }}</p>
     <button @click="setSaveDirectory">Choose Directory</button>
-    <input v-model="youtubeURL" type="text"/>
+    <input v-model="youtubeURL" type="text">
     <p>infoLoading: {{ infoLoading }}</p>
     <p>{{ loading }}</p>
     <div v-if="videoInfo">
       <label for="title">Title</label>
-      <input v-model="title" name="title" type="text"/>
+      <input v-model="title" name="title" type="text">
       <button @click="convert">Convert</button>
     </div>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
@@ -22,68 +22,102 @@ import fs from 'fs'
 import ytdl from 'ytdl-core'
 import HelloWorld from './components/HelloWorld.vue'
 
+const ffmpeg = window.require('fluent-ffmpeg')
+const binaries = window.require('ffmpeg-binaries')
+
 export default {
 	name: 'app',
 	components: {
 		HelloWorld
-  },
-  data() {
-    return {
-      youtubeURL: '',
-      savePath: localStorage.getItem('saveDirectory') ? localStorage.getItem('saveDirectory') : os.homedir(),
-      videoInfo: null,
-      infoLoading: false,
-      loading: 0,
-      title: '',
-    }
-  },
-  watch: {
-    youtubeURL() {
-      if (this.youtubeURL.length === 0) {
-        this.videoInfo = null
-        return
-      }
-      this.infoLoading = true
-      ytdl.getInfo(this.youtubeURL)
-      .then((info) => {
-        // .thumbnail_url, .title
-        this.loading = 0
-        this.videoInfo = info
-        this.title = info.title
-      }, (error) => {
-        this.videoInfo = null
-      })
-      .finally(() => this.infoLoading = false)
-    }
-  },
-  methods: {
-    setSaveDirectory() {
-      const directory = remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
-        title: 'Save Directory',
-        properties: ['openDirectory']
-      })
-      if (directory) {
-        this.savePath = directory[0]
-        localStorage.setItem('saveDirectory', directory[0])
-      }
-      // shell.showItemInFolder(remote.app.getPath('downloads'))
-    },
-    convert() {
-      console.log('convert', this.youtubeURL)
-      // import * as path from 'path'
-      // const outputPath = path.join(this.savePath, `tmp.mp3`);
-      const file = ytdl(this.youtubeURL, {filter: 'audioonly'})
+	},
+	data() {
+		return {
+			youtubeURL: '',
+			savePath: localStorage.getItem('saveDirectory')
+				? localStorage.getItem('saveDirectory')
+				: os.homedir(),
+			videoInfo: null,
+			infoLoading: false,
+			loading: 0,
+			title: '',
+			artist: 'test'
+		}
+	},
+	watch: {
+		youtubeURL() {
+			if (this.youtubeURL.length === 0) {
+				this.videoInfo = null
+				return
+			}
+			this.infoLoading = true
+			ytdl
+				.getInfo(this.youtubeURL)
+				.then(
+					info => {
+						// .thumbnail_url, .title
+						this.loading = 0
+						this.videoInfo = info
+						this.title = info.title
+					},
+					error => {
+						console.log(error)
+						this.videoInfo = null
+					}
+				)
+				.finally(() => (this.infoLoading = false))
+		}
+	},
+	methods: {
+		setSaveDirectory() {
+			const directory = remote.dialog.showOpenDialog(
+				remote.getCurrentWindow(),
+				{
+					title: 'Save Directory',
+					properties: ['openDirectory']
+				}
+			)
+			if (directory) {
+				this.savePath = directory[0]
+				localStorage.setItem('saveDirectory', directory[0])
+			}
+			// shell.showItemInFolder(remote.app.getPath('downloads'))
+		},
+		convert() {
+			console.log('convert', this.youtubeURL)
+			// import * as path from 'path'
+			// const outputPath = path.join(this.savePath, `tmp.mp3`);
+			const file = ytdl(this.youtubeURL, { filter: 'audioonly' })
 
-      file.on('progress', (chunk, downloaded, total) => {
-        this.loading = downloaded / total
-      })
+			file.on('progress', (chunk, downloaded, total) => {
+				this.loading = downloaded / total
+			})
 
-      file.pipe(fs.createWriteStream(`${this.savePath}/${this.title}.mp3`)).on('finish', () => {
-        this.loading = 100
-        console.log('we r done')
-      })
-    }
-  }
+			file
+				.pipe(fs.createWriteStream(`${this.savePath}/${this.title}_temp.mp3`))
+				.on('finish', () => {
+					this.loading = 100
+					this.addID3Tags(`${this.savePath}/${this.title}_temp.mp3`)
+					console.log('we r done')
+				})
+		},
+		addID3Tags(mp3Path) {
+			ffmpeg(mp3Path)
+				.setFfmpegPath(binaries.ffmpegPath())
+				.outputOptions('-metadata', `title=${this.title}`)
+				.outputOptions('-metadata', `artist=${this.artist}`)
+				.output(`${this.savePath}/${this.title}.mp3`)
+				// .audioBitrate(160)
+				.on('progress', (progress) => {
+					console.log(progress.percent)
+				})
+				.on('end', () => {
+					fs.unlink(`${this.savePath}/${this.title}_temp.mp3`, () => {
+						console.log('deleted temp')
+					})
+				})
+				.run()
+		}
+	}
 }
 </script>
 
