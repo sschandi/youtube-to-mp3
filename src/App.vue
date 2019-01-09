8f81117ff7dbde3d62ba7bb5a68c93c9cc4961d1
chandi <sukhdeep.29@live.ca>
2019-01-09 12:08:31 -0800

    added font + settings area

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
   <div id="app">
-		<!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-		<button class="btn" @click="setSaveDirectory">Choose Directory</button>
-		<button class="btn" @click="openSaveDirectory">Open Directory</button>
-		<p>{{ savePath }}</p>
-		<div class="input-container">
-			<div class="input">
-				<p>URL</p>
-				<input v-model="youtubeURL" for="url" type="text">
-			</div>
+		<div id="title">
+			<h1>Youtube to MP3</h1>
 		</div>
-		<p>infoLoading: {{ infoLoading }}</p>
-		<div v-if="videoInfo">
-			<p>{{ status }}: {{ loading }}</p>
+		<!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+		<div id="content">
 			<div class="input-container">
 				<div class="input">
-					<p>Title</p>
-					<input v-model="title" name="title" type="text">
+					<p>URL</p>
+					<input v-model="youtubeURL" for="url" type="text">
 				</div>
 			</div>
-			<div class="input-container">
-				<div class="input">
-					<p>Artist</p>
-					<input v-model="artist" name="artist" type="text">
-				</div>
+			<div v-if="infoLoading">
+				<p>Getting info...</p>
 			</div>
-			<button class="btn" @click="convert">Convert</button>
+			<button class="btn" @click="openSaveDirectory">Open Directory</button>
+			<div v-if="videoInfo">
+				<p>{{ status }}: {{ loading }}</p>
+				<div class="input-container">
+					<div class="input">
+						<p>Title</p>
+						<input v-model="title" name="title" type="text">
+					</div>
+				</div>
+				<div class="input-container">
+					<div class="input">
+						<p>Artist</p>
+						<input v-model="artist" name="artist" type="text">
+					</div>
+				</div>
+				<button class="btn" @click="convert">Convert</button>
+			</div>
+			<button class="btn btn-settings" @click="showSettings = !showSettings">
+				<i class="material-icons">settings</i>
+			</button>
+		</div>
+		<div id="settings" v-show="showSettings">
+			<p><span class="text-bold">Save Directory:</span> {{ savePath }}
+			<button class="btn" @click="setSaveDirectory">Change</button></p>
 		</div>
   </div>
 </template>
@@ -53,7 +65,8 @@ export default {
 			loading: 0,
 			status: 'Downloading',
 			title: '',
-			artist: ''
+			artist: '',
+			showSettings: false,
 		}
 	},
 	watch: {
@@ -149,11 +162,13 @@ export default {
 <style lang="scss">
 $white: #ffffff;
 $red: #FE2B53;
+$shadow: 0 10px 20px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+
 html, body {
 	margin: 0;
+	font-family: 'Comfortaa', cursive;
 }
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	height: 100vh;
@@ -161,6 +176,27 @@ html, body {
 	text-align: center;
 	color: $white;
 	background-image: linear-gradient(45deg, #312D44 0%, #7F69A1 99%, #8F75B1 100%);
+	display: flex;
+	flex-direction: column;
+}
+#title {
+	padding-top: 1rem;
+}
+#content {
+	position: relative;
+	flex-grow: 1;
+	padding-bottom: 1rem;
+}
+#settings {
+	background-color: $red;
+	box-shadow: $shadow;
+	.btn {
+		color: $red;
+		background-color: $white;
+	}
+}
+.text-bold {
+	font-weight: bold;
 }
 .btn {
 	padding: .5rem;
@@ -169,7 +205,19 @@ html, body {
 	background-color: $red;
 	color: $white;
 	border-radius: .25rem;
+	box-shadow: $shadow;
 	cursor: pointer;
+	&:focus {
+		outline: none;
+	}
+	&.btn-settings {
+		border-radius: 50%;
+		position: absolute;
+		bottom: 1rem;
+		right: 1rem;
+		width: 40px;
+		height: 40px;
+	}
 }
 .input-container {
 	display: flex;
